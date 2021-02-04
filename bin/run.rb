6f8a11b5905ef8385d1a42e0f474db4b88fd4b34
Nana <72888369+nanaama527@users.noreply.github.com>
2021-02-04 02:10:45 -0500

    commit tree 338c779f95ff3a10b4f691d1f337bc1f6f42da19

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,24 @@
-# require the environment file
+
+
+require_relative '../config/environment' 
+
+
+
+
+
+# require 'awesome_print'
+
+# array = [
+#     [0] 1,
+#     [1] 2,
+#     [2] 3,
+#     [3] 4
+# ] 
+
+# hash = {
+#     :a => 1,
+#     :b => 5
+# } 
+
+# ap array
+# ap hash
